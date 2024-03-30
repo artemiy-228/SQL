0dc842d78c0d05afcfa69145134caa3cc60a4277
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-03-30 10:34:19 +0500

    Another solution for Task_B

--- Select/Task_B.sql
+++ Select/Task_B.sql
@@ -13,3 +13,20 @@ JOIN Company
 WHERE YEAR(Dat) > 2012 AND CompanyName = 'S7'
 
 ORDER BY Dat
+
+/*
+The other way to get similar answer
+*/
+SELECT
+	(SELECT CityName FROM City WHERE Flight.CityFrom_ID = City.City_ID) as CityFrom,
+	(SELECT CityName FROM City WHERE Flight.CityTo_ID = City.City_ID) as CityTo,
+	Dat as [Date],
+	(SELECT PlaneTypeName FROM PlaneType WHERE PlaneType.PlaneType_ID = Flight.PlaneType_ID) AS Plane
+
+FROM Flight, Company
+
+WHERE YEAR(Dat) > 2012 AND
+	Company.Company_ID = Flight.Company_ID AND
+	Company.CompanyName = 'S7'
+
+ORDER BY Dat
