ea0ac848bc3c273528d8f7ce9cceb2292ba3b9b3
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-04-01 20:25:55 +0500

    Task 1_9 other solution

--- Select/Task_1_9.sql
+++ Select/Task_1_9.sql
@@ -13,3 +13,20 @@ WHERE CAST(Dat as DATE) = '20130427' AND
 	CityTo.CityName = 'Магадан'
 
 ORDER BY FIO
+
+/*
+The other solution(debatable)
+*/
+
+SELECT DISTINCT FIO
+FROM Passgr,
+	Flight,
+	(SELECT CityName AS CityFrom FROM City, Flight WHERE Flight.CityFrom_ID = City.City_ID) AS Cf,
+	(SELECT CityName AS CityTo FROM City, Flight WHERE Flight.CityTo_ID = City.City_ID) as Ct
+
+WHERE CAST(Dat AS DATE) = '20130427' AND
+	Passgr.Flight_ID = Flight.Flight_ID AND
+	CityFrom = 'Санкт-Петербург' AND
+	CityTo = 'Магадан'
+
+ORDER BY FIO
