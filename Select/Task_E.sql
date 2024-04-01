4850f2ee551585bbdb83ef9a9ae1c7900ff8c148
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-04-01 19:14:04 +0500

    Task_E

--- Select/Task_E.sql
+++ Select/Task_E.sql
@@ -11,3 +11,20 @@ WHERE PlaneTypeName = 'А-310' AND
 	  CityName = 'Санкт-Петербург'
 
 ORDER BY FIO
+
+/*
+The other way to solve this task
+*/
+
+SELECT DISTINCT FIO
+
+FROM Passgr, Flight, City, PlaneType
+
+WHERE Passgr.Flight_ID = Flight.Flight_ID AND
+	Flight.PlaneType_ID = PlaneType.PlaneType_ID AND
+	PlaneType.PlaneTypeName = 'А-310' AND
+	Flight.CityTo_ID = City.City_ID AND
+	City.CityName = 'Санкт-Петербург'
+
+
+ORDER BY FIO
