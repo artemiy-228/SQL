605d25a2ae54b1d06435d25cf8e64a3bbdd3c198
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-04-01 19:52:39 +0500

    Task 1_8 Other solution

--- Select/Task_1_8.sql
+++ Select/Task_1_8.sql
@@ -4,3 +4,14 @@ JOIN Seat
 	ON PlaneType.PlaneType_ID = Seat.PlaneType_ID
 
 GROUP BY PlaneType.PlaneTypeName
+
+/*
+The other solution
+*/
+
+SELECT PlaneType.PlaneTypeName AS Plane, COUNT(Seat.PlaneType_ID) AS NumOfSeat
+FROM PlaneType, Seat
+
+WHERE Seat.PlaneType_ID = PlaneType.PlaneType_ID
+
+GROUP BY PlaneType.PlaneTypeName
