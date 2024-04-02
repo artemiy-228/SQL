56b81d506a2ab73a73ff408478f1ce0363b5556f
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-04-02 22:25:49 +0500

    Task 1_7 another solution

--- Select/Task_1_7.sql
+++ Select/Task_1_7.sql
@@ -7,3 +7,17 @@ RIGHT JOIN City
 WHERE Flight_ID IS NULL
 
 ORDER BY CityName
+
+/*
+Another solution with quantor of existance
+*/
+
+SELECT CityName AS City
+FROM City
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Flight
+    WHERE Flight.CityTo_ID = City.City_ID
+)
+ORDER BY CityName;
+
