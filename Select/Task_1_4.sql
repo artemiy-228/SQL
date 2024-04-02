91de6b144401136fc5619774a1b47e092afc2bf4
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-04-03 00:24:45 +0500

    Task 1_4 another solution

--- Select/Task_1_4.sql
+++ Select/Task_1_4.sql
@@ -3,3 +3,14 @@ FROM City
 LEFT JOIN Flight
 	ON City.City_ID = Flight.CityFrom_ID
 GROUP BY City.CityName
+
+/*
+The other solution
+*/
+
+SELECT CityName AS City, COUNT(Flight.Flight_ID) AS AmountOfFlying
+FROM Flight
+RIGHT JOIN City
+	ON Flight.CityFrom_ID = City.City_ID
+
+GROUP BY City.CityName
