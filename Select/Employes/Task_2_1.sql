094c3b606dfc75f111f6bef78c243ac824f472bf
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-04-04 15:47:09 +0500

    Task 2_1 other solution

--- Select/Employes/Task_2_1.sql
+++ Select/Employes/Task_2_1.sql
@@ -1,2 +1,9 @@
 SELECT AVG(DATEDIFF(day, Birth, GETDATE())) AS Days
 FROM Employee
+/*
+The other solution
+*/
+SELECT SUM(DATEDIFF(day, Birth, GETDATE())) / COUNT(DATEDIFF(day, Birth, GETDATE()))   AS Days
+FROM Employee
+
+
