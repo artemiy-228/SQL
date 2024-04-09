a997ed55cf0b3c2261800b27e25bd4e86e4a6960
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-04-10 00:11:14 +0500

    Task 1_14 other solution

--- Select/Aeroflot/Task_1_14.sql
+++ Select/Aeroflot/Task_1_14.sql
@@ -11,3 +11,23 @@ WHERE AmountOfPlaces = (
 	SELECT MAX(AmountOfPlaces)
 	FROM Flights
 )
+ORDER BY Plane
+/*
+The other solution
+*/
+
+WITH Flights AS (
+    SELECT PT.PlaneTypeName AS Plane, COUNT(S.PlaneType_ID) AS AmountOfPlaces
+    FROM PlaneType AS PT
+    JOIN Seat AS S
+        ON PT.PlaneType_ID = S.PlaneType_ID
+    GROUP BY PT.PlaneType_ID, PT.PlaneTypeName
+)
+
+SELECT Plane
+FROM Flights
+WHERE AmountOfPlaces = (
+	SELECT MAX(AmountOfPlaces)
+	FROM Flights
+)
+ORDER BY Plane
